8e48fc5e1df6334f6e3f7b0cd551aed7cd1e58a9
Eamonn de Leastar <edeleastar@wit.ie>
2019-03-01 18:21:15 +0000

    credential and image upload handlers

--- app/controllers/gallery.js
+++ app/controllers/gallery.js
@@ -5,6 +5,20 @@ const Gallery = {
     handler: async function(request, h) {
       return h.view('gallery', { title: 'Cloudinary Gallery' });
     }
+  },
+  updateCredentials: {
+    handler: async function(request, h) {
+      console.log(`credentials - name: ${request.payload.name} `);
+      console.log(`credentials - key: ${request.payload.key} `);
+      console.log(`credentials - secret: ${request.payload.secret} `);
+      return h.redirect('/');
+    }
+  },
+  uploadFile: {
+    handler: async function(request, h) {
+      console.log(`credentials - secret: ${request.payload.title} `);
+      return h.redirect('/');
+    }
   }
 };
 
