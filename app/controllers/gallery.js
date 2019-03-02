bd9500f2698798a001f5a9f7b49da43d1500c279
Eamonn de Leastar <edeleastar@wit.ie>
2019-03-02 17:55:38 +0000

    request all assets and log image urls to console

--- app/controllers/gallery.js
+++ app/controllers/gallery.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const cloudinary = require('cloudinary');
+
 const Gallery = {
   index: {
     handler: async function(request, h) {
@@ -11,10 +13,20 @@ const Gallery = {
   },
   updateCredentials: {
     handler: async function(request, h) {
-      this.cloudinaryCredentials.name = request.payload.name;
-      this.cloudinaryCredentials.key = request.payload.key;
-      this.cloudinaryCredentials.secret = request.payload.secret;
-      return h.redirect('/');
+      this.cloudinaryCredentials.cloud_name = request.payload.name;
+      this.cloudinaryCredentials.api_key = request.payload.key;
+      this.cloudinaryCredentials.api_secret = request.payload.secret;
+
+      cloudinary.config(this.cloudinaryCredentials);
+      const result = await cloudinary.v2.api.resources();
+      result.resources.forEach(image => {
+        console.log(image.url);
+      });
+      return h.view('gallery', {
+        title: 'Cloudinary Gallery',
+        cloudinary: this.cloudinaryCredentials,
+        images: result.resources.images
+      });
     }
   },
   uploadFile: {
